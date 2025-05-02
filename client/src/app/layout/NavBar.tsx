fceaa4cf4e9bea0b1b5f51ce74f5386d97aa6bdb
mercer-mk <milanckhunt@gmail.com>
2025-05-01 21:47:21 -0400

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -6,20 +6,22 @@ import {
   Typography,
   Container,
   MenuItem,
+  LinearProgress,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/componentes/MenuItemLink";
+import { Observer } from "mobx-react-lite";
+import { useStore } from "../../lib/hooks/useStore";
 
 export default function NavBar() {
+  const { uiStore } = useStore();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundImage:
-            "linear-gradient(135deg,#182a73 0%, #218aae 69%, #20a7ac 89%)",
-        }}
-      >
+          <AppBar position="static"
+                 sx={{
+                     backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+                     position: 'relative'
+                 }}>
         <Container maxWidth="xl">
           <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
             <Box sx={{ display: "flex" }}>
@@ -37,10 +39,26 @@ export default function NavBar() {
             <Box sx={{ display: "flex" }}>
               <MenuItemLink to="/activities">Activities</MenuItemLink>
               <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
+              <MenuItemLink to="/counter">Counter</MenuItemLink>
             </Box>
             <MenuItem>User Menu</MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+                     {() => uiStore.isLoading ? (
+                         <LinearProgress
+                             color="secondary"
+                             sx={{
+                                 position: 'absolute',
+                                 bottom: 0,
+                                 left: 0,
+                                 right: 0,
+                                 height: 4
+                             }}
+                         />
+                     ) : null}
+                 </Observer>
       </AppBar>
     </Box>
   );
