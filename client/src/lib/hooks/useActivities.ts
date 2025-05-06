122291fffe2ed4e75f3f2c9af16ef39493b5f01b
mercer-mk <milanckhunt@gmail.com>
2025-05-06 12:41:43 -0400

    Added user form sec 13

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,17 +1,21 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
+import { useAccount } from "./useAccounts";
 
 export const useActivities = (id?:string) => {
   const queryClient = useQueryClient();
   const location = useLocation();
-  const { data: activities, isPending } = useQuery({
+  const {currentUser} = useAccount();
+
+
+  const { data: activities, isLoading } = useQuery({
     queryKey: ["activities"],
     queryFn: async () => {
       const response = await agent.get<Activity[]>("/activities");
       return response.data;
     },
-    enabled: !id && location.pathname === '/activities'
+    enabled: !id && location.pathname === '/activities' && !!currentUser
   });
   const { data: activity, isLoading:isLoadingActivity} = useQuery({
     queryKey: ["activities",id],
@@ -19,7 +23,7 @@ export const useActivities = (id?:string) => {
       const response = await agent.get<Activity>(`/activities/${id}`);
       return response.data;
     },
-    enabled:!!id
+    enabled: !!id && !!currentUser
   });
 
   const updateActivity = useMutation({
@@ -57,7 +61,7 @@ export const useActivities = (id?:string) => {
 
   return {
     activities,
-    isPending,
+    isLoading,
     updateActivity,
     createActivity,
     deleteActivity,
