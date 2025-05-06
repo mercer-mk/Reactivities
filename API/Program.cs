b388c20ee0b5466e6826a9992f8f3acb2194b6e1
mercer-mk <milanckhunt@gmail.com>
2025-05-06 11:23:45 -0400

    End of identity section

--- API/Program.cs
+++ API/Program.cs
@@ -6,41 +6,64 @@ using Application.Core;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Microsoft.AspNetCore.Identity;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt => {
+builder.Services.AddControllers(opt =>
+ {
+     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+     opt.Filters.Add(new AuthorizeFilter(policy));
+ });
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddCors();
-builder.Services.AddMediatR(x => {
-     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
-     x.AddOpenBehavior(typeof(ValidationBehavior<,>));
- });
+builder.Services.AddMediatR(x =>
+{
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+ {
+     opt.User.RequireUniqueEmail = true;
+ })
+ .AddRoles<IdentityRole>()
+ .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
-app.MapControllers();
+app.UseCors(x => x.AllowAnyHeader().
+AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
-using var scope =app.Services.CreateScope();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>(); // api/login
+
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
-try{
+try
+{
     var context = services.GetRequiredService<AppDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
     await context.Database.MigrateAsync();
-    await DbIntializer.SeedData(context);
+    await DbIntializer.SeedData(context, userManager);
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during db migration");
